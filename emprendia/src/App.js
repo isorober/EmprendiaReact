d8c2bd85ea45f1e069404b0e14ebd62ba64bf185
isorober <robvenbac@hotmail.com>
2024-01-22 16:53:04 -0700

    Included the rest of the main page content

--- emprendia/src/App.js
+++ emprendia/src/App.js
@@ -6,6 +6,25 @@ class App extends Component{
   render() {
     return (
       <div className="App">
+        <head>
+<title>Emprendia</title>
+</head>
+<body>
+<header>
+<div id="MyHeader">
+    <a>Home</a>
+    <a>About us</a>
+    <a>Log in</a>
+</div>
+</header>
+<h1>DESCUBRE EL FUTURO
+    DE TU NEGOCIO CON IA
+</h1>
+<div id="botones">
+<button>Try Now</button>
+<button>Watch Video</button>
+</div>
+</body>
       </div>
     );
   }
